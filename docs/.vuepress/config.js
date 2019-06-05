f0cabf97bc984fed9cb27b23746f3ad523ad543c
busyrat <1435858847@qq.com>
2019-06-05 11:25:32 +0800

    feat: nav和sidebar自动生成

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,46 +1,17 @@
-const { genSideBar } = require('./utils')
+const { genNavAndSidebar } = require('./utils')
 module.exports = {
   base: "/blog/",
   title: "busyrat blog",
   themeConfig: {
-    // 导航栏
-    nav: [
-      { text: 'fe', 
-        items: [
-          { text: 'library', link: '/fe/library/' },
-          { text: 'ie9', link: '/fe/ie9/' },
-          { text: '知识点', link: '/fe/知识点/' },
-        ]
-      },
-      { text: 'op', 
-        items: [
-          { text: 'docker', link: '/op/docker/' },
-          { text: 'linux', link: '/op/linux' },
-          { text: 'gitlab-ci', link: '/op/gitlab-ci' },
-        ]
-      },
-      {
-        text: 'be',
-        items: [
-          { text: 'Node', link: '/be/node' },
-          { text: 'Redis', link: '/be/Redis' },
-        ]
-      },
-      {
-        text: 'others',
-        items: [
-          { text: '《重学前端》', link: '/others/《重学前端》' },
-          { text: 'Git', link: '/others/Git' },
-          { text: '工具集', link: '/others/工具集' },
-        ]
-      },
-      { text: 'github', link: 'https://github.com/busyrat/blog' },
-    ],
-    // 侧边栏
-    sidebar: {
-      ...genSideBar('/fe/library/'),
-      ...genSideBar('/op/docker/'),
-    },
+    ...genNavAndSidebar(),
+    // nav: { text: 'fe', 
+    //   items: [
+    //     { text: 'library', link: '/fe/library/' }
+    //   ]
+    // },
+    // sidebar: {
+    //   '/fe/library/': [['vue', 'vue']]
+    // },
 
     editLinks: true,
     lastUpdated: "上次更新",
--- docs/.vuepress/utils.js
+++ docs/.vuepress/utils.js
@@ -1,28 +1,72 @@
-const glob = require('glob')
-const path = require('path')
+const glob = require("glob");
+const path = require("path");
+const fs = require("fs");
 
-const DOCS_DIR = dir => path.join(__dirname, '../', dir)
+const DOCS_DIR = (dir = "") => path.join(__dirname, "../", dir);
 
-exports.genSideBar = function (dirs) {
-  if(typeof dirs === 'string') {
-    dirs = [dirs]
+const genSideBar = function(dirs) {
+  if (typeof dirs === "string") {
+    dirs = [dirs];
   }
-  let ret = {}
+  let ret = {};
   dirs.forEach(dir => {
-    const pages = glob.sync(`${DOCS_DIR(dir)}/*.md`)
+    const pages = glob.sync(`${DOCS_DIR(dir)}/*.md`);
     // ret[dir] = [{
     //   title: dir,
     //   collapsable: false,
     //   sidebarDepth: 2,
     //   children: []
     // }]
-    ret[dir] = []
+    ret[dir] = [];
     pages.forEach(page => {
-      let pageName = page.match(/\/([^\/]+)\.md/)[1]
-      if (pageName === 'README') return
-      ret[dir].push([pageName, pageName])
-    })
-  })
-  
-  return ret
-}
+      let pageName = page.match(/\/([^\/]+)\.md/)[1];
+      if (pageName === "README") return;
+      ret[dir].push([pageName, pageName]);
+    });
+  });
+
+  return ret;
+};
+
+exports.genNavAndSidebar = function() {
+  let ret = {
+    nav: [],
+    sidebar: {}
+  };
+  const childs = glob.sync(`${DOCS_DIR()}/*`);
+  childs.forEach(child => {
+    let isDir = fs.statSync(child).isDirectory();
+    if (isDir) {
+      let nav = {
+        text: child.match(/\/([^\/]+)$/)[1],
+        items: []
+      };
+      let subChilds = glob.sync(`${child}/*`);
+      subChilds.forEach(subChild => {
+        let isDir = fs.statSync(subChild).isDirectory();
+        let text;
+        let link;
+        if (isDir) {
+          text = subChild.match(/\/([^\/]+)$/)[1];
+          link = `/${nav.text}/${text}/`;
+          ret.sidebar = {
+            ...ret.sidebar,
+            ...genSideBar(link)
+          };
+        } else {
+          if (/.md/.test(subChild)) {
+            text = subChild.match(/\/([^\/]+).md/)[1];
+            link = `/${nav.text}/${text}`;
+          } else {
+            return
+          }
+        }
+
+        nav.items.push({ text, link });
+      });
+      ret.nav.push(nav);
+    }
+  });
+  ret.nav.push({ text: "github", link: "https://github.com/busyrat/blog" });
+  return ret;
+};
